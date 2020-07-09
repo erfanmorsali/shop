07f0b6848dca33b53c13cb97862d03e4029d0490
erfanmorsali <erfanmorsali78@gmail.com>
2020-07-09 12:31:59 +0430

    add paginating features and update some features

--- eshop_products/views.py
+++ eshop_products/views.py
@@ -9,10 +9,14 @@ from .models import Product
 class ProductList(ListView):
     queryset = Product.objects.get_active_product()
     template_name = 'products/product_list.html'
+    paginate_by = 3
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super(ProductList, self).get_context_data(*args, **kwargs)
+        return context
 
 
 class ProductDetail(DetailView):
     queryset = Product.objects.get_active_product()
     template_name = 'products/product_detail.html'
-
 
